338ca20cc4ef34e53f27c1623902af95e216eb5f
Janne Sinivirta <janne.sinivirta@gmail.com>
2026-01-01 12:35:22 +0200

    Improve exec package test coverage to 100%
    - Make execFunc injectable for testing
    - Add Unix-specific tests that mock syscall.Exec
    - Test argument passing, binary resolution, error handling

--- pkg/exec/exec_unix.go
+++ pkg/exec/exec_unix.go
@@ -6,6 +6,10 @@ import (
 	"syscall"
 )
 
+// execFunc is the function used to exec into a new process.
+// Can be overridden for testing.
+var execFunc = syscall.Exec
+
 // Exec replaces the current process with the specified command.
 // This is the Unix implementation using syscall.Exec.
 func (e *RealExecutor) Exec(name string, args []string) error {
@@ -17,7 +21,9 @@ func (e *RealExecutor) Exec(name string, args []string) error {
 	// syscall.Exec replaces the current process.
 	// argv[0] must be the program name by convention.
 	argv := append([]string{name}, args...)
+	env := environ()
+
 	// #nosec G204 -- This is intentional: exec mode allows users to specify the command to run after checks pass.
 	// The command comes from CLI args which are under user control.
-	return syscall.Exec(binary, argv, environ())
+	return execFunc(binary, argv, env)
 }
